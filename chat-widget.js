6d781f164d9710c324905fa77247b29848f4f15c
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-06-29 14:50:16 +0300

    infra: tunnel URL 2026-06-29 14:50

--- chat-widget.js
+++ chat-widget.js
@@ -7,7 +7,7 @@
 (function () {
   'use strict';
 
-  var DEFAULT_SERVER = 'https://disclaimers-alt-distributors-area.trycloudflare.com'; // подменяется start_server.ps1 на туннель
+  var DEFAULT_SERVER = 'https://acknowledged-visit-incidence-earned.trycloudflare.com'; // подменяется start_server.ps1 на туннель
   var serverUrl = null;
 
   function detectReportId() {
