6446b1648583d48111d516b0d976d9b2d6116e95
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-06-29 15:14:56 +0300

    infra: tunnel URL 2026-06-29 15:14

--- chat-widget.js
+++ chat-widget.js
@@ -7,7 +7,7 @@
 (function () {
   'use strict';
 
-  var DEFAULT_SERVER = 'https://subsection-policies-replace-arrival.trycloudflare.com'; // подменяется start_server.ps1 на туннель
+  var DEFAULT_SERVER = 'https://clubs-teeth-guaranteed-trackback.trycloudflare.com'; // подменяется start_server.ps1 на туннель
   var serverUrl = null;
 
   function detectReportId() {
