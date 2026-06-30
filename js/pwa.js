54340460fd83cef8b06015063ffb65bc08b295c7
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-06-30 08:00:20 +0300

    infra: tunnel URL 2026-06-30 08:00

--- js/pwa.js
+++ js/pwa.js
@@ -6,7 +6,7 @@
 (function (global) {
   'use strict';
   var SW_PATH = 'sw.js';
-  var DEFAULT_SERVER = 'https://proceeding-wrapping-knowing-welfare.trycloudflare.com';
+  var DEFAULT_SERVER = 'https://grab-leo-suburban-specializing.trycloudflare.com';
   var serverUrl = null;
   var vapidKey = null;
   var installPrompt = null;
