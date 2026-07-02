8e7f26de37010d02bc623b257354bff2b343907f
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-07-02 06:00:12 +0300

    infra: tunnel URL 2026-07-02 06:00

--- js/pwa.js
+++ js/pwa.js
@@ -6,7 +6,7 @@
 (function (global) {
   'use strict';
   var SW_PATH = 'sw.js';
-  var DEFAULT_SERVER = 'https://pupils-exhibitions-angela-medieval.trycloudflare.com';
+  var DEFAULT_SERVER = 'https://stack-television-regularly-nest.trycloudflare.com';
   var serverUrl = null;
   var vapidKey = null;
   var installPrompt = null;
