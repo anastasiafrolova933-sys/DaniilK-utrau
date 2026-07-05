3da67128fcae637bc6de9cb81cb502edde33b1b3
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-07-05 06:00:12 +0300

    infra: tunnel URL 2026-07-05 06:00

--- chat-widget.js
+++ chat-widget.js
@@ -7,7 +7,7 @@
 (function () {
   'use strict';
 
-  var DEFAULT_SERVER = 'https://interview-feb-acoustic-chains.trycloudflare.com'; // подменяется start_server.ps1 на туннель
+  var DEFAULT_SERVER = 'https://recommended-schedules-consistency-undergraduate.trycloudflare.com'; // подменяется start_server.ps1 на туннель
   var serverUrl = null;
 
   function detectReportId() {
--- js/pwa.js
+++ js/pwa.js
@@ -6,7 +6,7 @@
 (function (global) {
   'use strict';
   var SW_PATH = 'sw.js';
-  var DEFAULT_SERVER = 'https://interview-feb-acoustic-chains.trycloudflare.com';
+  var DEFAULT_SERVER = 'https://recommended-schedules-consistency-undergraduate.trycloudflare.com';
   var serverUrl = null;
   var vapidKey = null;
   var installPrompt = null;
