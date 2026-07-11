2f5b066637a91983b059d494dc72694ee44e725c
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-07-11 06:00:14 +0300

    infra: tunnel URL 2026-07-11 06:00

--- chat-widget.js
+++ chat-widget.js
@@ -7,7 +7,7 @@
 (function () {
   'use strict';
 
-  var DEFAULT_SERVER = 'https://amplifier-extras-nationally-boss.trycloudflare.com'; // подменяется start_server.ps1 на туннель
+  var DEFAULT_SERVER = 'https://ins-wider-water-regarded.trycloudflare.com'; // подменяется start_server.ps1 на туннель
   var serverUrl = null;
 
   function detectReportId() {
