9e0e1504011318654002cf0df17da387b0c6b863
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-07-18 06:00:12 +0300

    infra: tunnel URL 2026-07-18 06:00

--- js/pwa.js
+++ js/pwa.js
@@ -6,7 +6,7 @@
 (function (global) {
   'use strict';
   var SW_PATH = 'sw.js';
-  var DEFAULT_SERVER = 'https://calculator-nicole-jeans-shall.trycloudflare.com';
+  var DEFAULT_SERVER = 'https://federal-bronze-beings-meetup.trycloudflare.com';
   var serverUrl = null;
   var vapidKey = null;
   var installPrompt = null;
