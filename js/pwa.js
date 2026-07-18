35413e9cae8d38faa92269d02e9474a5023a7c2f
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-07-18 19:45:44 +0300

    infra: tunnel URL 2026-07-18 19:45

--- js/pwa.js
+++ js/pwa.js
@@ -6,7 +6,7 @@
 (function (global) {
   'use strict';
   var SW_PATH = 'sw.js';
-  var DEFAULT_SERVER = 'https://rent-affiliate-kai-ordered.trycloudflare.com';
+  var DEFAULT_SERVER = 'https://api.trycloudflare.com';
   var serverUrl = null;
   var vapidKey = null;
   var installPrompt = null;
