fc663ce3285d595bc74db9b0b7cc4f1d99a31ca8
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-07-20 12:15:13 +0300

    infra: tunnel URL 2026-07-20 12:15

--- chat-widget.js
+++ chat-widget.js
@@ -7,7 +7,7 @@
 (function () {
   'use strict';
 
-  var DEFAULT_SERVER = 'https://api.trycloudflare.com'; // подменяется start_server.ps1 на туннель
+  var DEFAULT_SERVER = 'https://external-vertical-lace-princess.trycloudflare.com'; // подменяется start_server.ps1 на туннель
   var serverUrl = null;
 
   function detectReportId() {
