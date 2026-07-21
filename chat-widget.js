2681ef32cf08c75073908e5059bbb455f450ca9b
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-07-21 04:15:11 +0300

    infra: tunnel URL 2026-07-21 04:15

--- chat-widget.js
+++ chat-widget.js
@@ -7,7 +7,7 @@
 (function () {
   'use strict';
 
-  var DEFAULT_SERVER = 'https://was-sale-bold-auctions.trycloudflare.com'; // подменяется start_server.ps1 на туннель
+  var DEFAULT_SERVER = 'https://api.trycloudflare.com'; // подменяется start_server.ps1 на туннель
   var serverUrl = null;
 
   function detectReportId() {
