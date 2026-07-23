fb72cc635c1e40fac82d4aa17be9a92c26c0b793
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-07-23 06:00:13 +0300

    infra: tunnel URL 2026-07-23 06:00

--- js/pwa.js
+++ js/pwa.js
@@ -6,7 +6,7 @@
 (function (global) {
   'use strict';
   var SW_PATH = 'sw.js';
-  var DEFAULT_SERVER = 'https://uniform-files-spouse-capable.trycloudflare.com';
+  var DEFAULT_SERVER = 'https://pose-publishers-sterling-usr.trycloudflare.com';
   var serverUrl = null;
   var vapidKey = null;
   var installPrompt = null;
