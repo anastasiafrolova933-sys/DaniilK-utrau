94331c73272659132d13f3b5ae0b870ab3756160
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-07-23 21:25:11 +0300

    infra: tunnel URL 2026-07-23 21:25

--- chat-widget.js
+++ chat-widget.js
@@ -7,7 +7,7 @@
 (function () {
   'use strict';
 
-  var DEFAULT_SERVER = 'https://faq-sport-aware-negotiations.trycloudflare.com'; // подменяется start_server.ps1 на туннель
+  var DEFAULT_SERVER = 'https://api.trycloudflare.com'; // подменяется start_server.ps1 на туннель
   var serverUrl = null;
 
   function detectReportId() {
