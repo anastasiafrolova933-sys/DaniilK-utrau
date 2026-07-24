7b27a08d91aea76c427fa8860e846dfbce0dace5
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-07-24 06:00:13 +0300

    infra: tunnel URL 2026-07-24 06:00

--- js/pwa.js
+++ js/pwa.js
@@ -6,7 +6,7 @@
 (function (global) {
   'use strict';
   var SW_PATH = 'sw.js';
-  var DEFAULT_SERVER = 'https://headquarters-thinks-queensland-males.trycloudflare.com';
+  var DEFAULT_SERVER = 'https://direct-render-singles-helping.trycloudflare.com';
   var serverUrl = null;
   var vapidKey = null;
   var installPrompt = null;
