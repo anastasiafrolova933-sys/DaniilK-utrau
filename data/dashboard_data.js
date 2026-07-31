76c47d42f65fe13b19912455f97fb6fd0cac0aa4
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-07-31 10:00:34 +0300

    Data update 31.07.2026 10:00

--- data/dashboard_data.js
+++ data/dashboard_data.js
@@ -1,5 +1,5 @@
-﻿// Auto-generated 2026-07-30 10:00 - do not edit
-window.UTRAU_DATA={generated:"2026-07-30 10:00",daily:[
+﻿// Auto-generated 2026-07-31 10:00 - do not edit
+window.UTRAU_DATA={generated:"2026-07-31 10:00",daily:[
   {"date":"2024-01-01","year":2024,"revenue":1662980,"roomNights":40,"guestArrivals":0,"roomArrivals":0,"adr":41574.5,"revpar":35382.55,"als":0,"totalRooms":52,"underRepair":5,"availRooms":47,"occupancy":85.11},
   {"date":"2024-01-02","year":2024,"revenue":272640,"roomNights":10,"guestArrivals":27,"roomArrivals":10,"adr":27264,"revpar":5926.96,"als":2,"totalRooms":52,"underRepair":6,"availRooms":46,"occupancy":21.74},
   {"date":"2024-01-03","year":2024,"revenue":430140,"roomNights":16,"guestArrivals":33,"roomArrivals":11,"adr":26883.75,"revpar":9558.67,"als":1.45,"totalRooms":52,"underRepair":7,"availRooms":45,"occupancy":35.56},
@@ -939,5 +939,6 @@ window.UTRAU_DATA={generated:"2026-07-30 10:00",daily:[
   {"date":"2026-07-25","year":2026,"revenue":663840,"roomNights":54,"guestArrivals":106,"roomArrivals":54,"adr":12293.33,"revpar":12293.33,"als":1,"totalRooms":54,"underRepair":0,"availRooms":54,"occupancy":100},
   {"date":"2026-07-26","year":2026,"revenue":633252.5,"roomNights":52,"guestArrivals":138,"roomArrivals":52,"adr":12177.93,"revpar":11948.16,"als":1.17,"totalRooms":54,"underRepair":1,"availRooms":53,"occupancy":98.11},
   {"date":"2026-07-27","year":2026,"revenue":604558.5,"roomNights":52,"guestArrivals":114,"roomArrivals":41,"adr":11626.13,"revpar":11195.53,"als":1.66,"totalRooms":54,"underRepair":0,"availRooms":54,"occupancy":96.3},
-  {"date":"2026-07-28","year":2026,"revenue":637698.5,"roomNights":52,"guestArrivals":98,"roomArrivals":33,"adr":12263.43,"revpar":11809.23,"als":2.18,"totalRooms":54,"underRepair":0,"availRooms":54,"occupancy":96.3}
+  {"date":"2026-07-28","year":2026,"revenue":637698.5,"roomNights":52,"guestArrivals":98,"roomArrivals":33,"adr":12263.43,"revpar":11809.23,"als":2.18,"totalRooms":54,"underRepair":0,"availRooms":54,"occupancy":96.3},
+  {"date":"2026-07-29","year":2026,"revenue":536837,"roomNights":48,"guestArrivals":61,"roomArrivals":20,"adr":11184.1,"revpar":9941.43,"als":1.25,"totalRooms":54,"underRepair":0,"availRooms":54,"occupancy":88.89}
 ]};
