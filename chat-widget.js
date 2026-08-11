5835b72762b1d4bf8ca66f098c0c94a84d6b302b
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-08-11 06:00:12 +0300

    infra: tunnel URL 2026-08-11 06:00

--- chat-widget.js
+++ chat-widget.js
@@ -7,7 +7,7 @@
 (function () {
   'use strict';
 
-  var DEFAULT_SERVER = 'https://rentals-published-asbestos-nec.trycloudflare.com'; // подменяется start_server.ps1 на туннель
+  var DEFAULT_SERVER = 'https://plastic-institutions-networks-independent.trycloudflare.com'; // подменяется start_server.ps1 на туннель
   var serverUrl = null;
 
   function detectReportId() {
