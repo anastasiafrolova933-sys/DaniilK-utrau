6744024fbfbfc98079796f6c71d929ae8c0e3d57
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-08-12 06:00:16 +0300

    infra: tunnel URL 2026-08-12 06:00

--- js/pwa.js
+++ js/pwa.js
@@ -6,7 +6,7 @@
 (function (global) {
   'use strict';
   var SW_PATH = 'sw.js';
-  var DEFAULT_SERVER = 'https://plastic-institutions-networks-independent.trycloudflare.com';
+  var DEFAULT_SERVER = 'https://charm-general-extends-flash.trycloudflare.com';
   var serverUrl = null;
   var vapidKey = null;
   var installPrompt = null;
