491c7735c060521767d4ae4f041dcb92eac3f438
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-08-22 12:35:24 +0300

    infra: tunnel URL 2026-08-22 12:35

--- chat-widget.js
+++ chat-widget.js
@@ -7,7 +7,7 @@
 (function () {
   'use strict';
 
-  var DEFAULT_SERVER = 'https://contracts-rolling-tab-supplement.trycloudflare.com'; // подменяется start_server.ps1 на туннель
+  var DEFAULT_SERVER = 'https://api.trycloudflare.com'; // подменяется start_server.ps1 на туннель
   var serverUrl = null;
 
   function detectReportId() {
