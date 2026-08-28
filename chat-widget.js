fdcb2ea1be77a8a21b65f1dd094aed3edd6cbeae
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-08-28 08:15:49 +0300

    infra: tunnel URL 2026-08-28 08:15

--- chat-widget.js
+++ chat-widget.js
@@ -7,7 +7,7 @@
 (function () {
   'use strict';
 
-  var DEFAULT_SERVER = 'https://cruises-attributes-register-valuation.trycloudflare.com'; // подменяется start_server.ps1 на туннель
+  var DEFAULT_SERVER = 'https://api.trycloudflare.com'; // подменяется start_server.ps1 на туннель
   var serverUrl = null;
 
   function detectReportId() {
