4473130730f87ca963bac77680738ee2973c964d
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-08-29 06:00:13 +0300

    infra: tunnel URL 2026-08-29 06:00

--- chat-widget.js
+++ chat-widget.js
@@ -7,7 +7,7 @@
 (function () {
   'use strict';
 
-  var DEFAULT_SERVER = 'https://offers-ruby-summit-citizens.trycloudflare.com'; // подменяется start_server.ps1 на туннель
+  var DEFAULT_SERVER = 'https://existed-calendar-packed-meaningful.trycloudflare.com'; // подменяется start_server.ps1 на туннель
   var serverUrl = null;
 
   function detectReportId() {
