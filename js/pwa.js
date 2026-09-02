e67c3534f25eeb2cb1d1d1139b0b0bc532d7eae7
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-09-02 06:00:12 +0300

    infra: tunnel URL 2026-09-02 06:00

--- js/pwa.js
+++ js/pwa.js
@@ -6,7 +6,7 @@
 (function (global) {
   'use strict';
   var SW_PATH = 'sw.js';
-  var DEFAULT_SERVER = 'https://met-weddings-scores-things.trycloudflare.com';
+  var DEFAULT_SERVER = 'https://clark-pete-direction-architectural.trycloudflare.com';
   var serverUrl = null;
   var vapidKey = null;
   var installPrompt = null;
