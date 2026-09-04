c82cf6fde7dd088026bf272134700275f74eb358
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-09-04 06:00:15 +0300

    infra: tunnel URL 2026-09-04 06:00

--- chat-widget.js
+++ chat-widget.js
@@ -7,7 +7,7 @@
 (function () {
   'use strict';
 
-  var DEFAULT_SERVER = 'https://fountain-brand-organisations-institutional.trycloudflare.com'; // подменяется start_server.ps1 на туннель
+  var DEFAULT_SERVER = 'https://coins-advanced-walk-turner.trycloudflare.com'; // подменяется start_server.ps1 на туннель
   var serverUrl = null;
 
   function detectReportId() {
