0a39bd9fcef0475b6afa511d2c740b7155134974
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-09-12 02:35:21 +0300

    infra: tunnel URL 2026-09-12 02:35

--- chat-widget.js
+++ chat-widget.js
@@ -7,7 +7,7 @@
 (function () {
   'use strict';
 
-  var DEFAULT_SERVER = 'https://gravity-work-terrorist-responded.trycloudflare.com'; // подменяется start_server.ps1 на туннель
+  var DEFAULT_SERVER = 'https://tile-url-faq-harrison.trycloudflare.com'; // подменяется start_server.ps1 на туннель
   var serverUrl = null;
 
   function detectReportId() {
--- js/pwa.js
+++ js/pwa.js
@@ -6,7 +6,7 @@
 (function (global) {
   'use strict';
   var SW_PATH = 'sw.js';
-  var DEFAULT_SERVER = 'https://gravity-work-terrorist-responded.trycloudflare.com';
+  var DEFAULT_SERVER = 'https://tile-url-faq-harrison.trycloudflare.com';
   var serverUrl = null;
   var vapidKey = null;
   var installPrompt = null;
