df4d42bdd2b718b1e688f962a251a3463f14df27
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-09-11 06:00:13 +0300

    infra: tunnel URL 2026-09-11 06:00

--- js/pwa.js
+++ js/pwa.js
@@ -6,7 +6,7 @@
 (function (global) {
   'use strict';
   var SW_PATH = 'sw.js';
-  var DEFAULT_SERVER = 'https://offerings-leslie-magic-hood.trycloudflare.com';
+  var DEFAULT_SERVER = 'https://gravity-work-terrorist-responded.trycloudflare.com';
   var serverUrl = null;
   var vapidKey = null;
   var installPrompt = null;
