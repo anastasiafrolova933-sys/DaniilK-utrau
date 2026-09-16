bb24827282d5ca66a868f45ec686026e0e0f7c25
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-09-16 06:00:13 +0300

    infra: tunnel URL 2026-09-16 06:00

--- js/pwa.js
+++ js/pwa.js
@@ -6,7 +6,7 @@
 (function (global) {
   'use strict';
   var SW_PATH = 'sw.js';
-  var DEFAULT_SERVER = 'https://perry-jun-delaware-color.trycloudflare.com';
+  var DEFAULT_SERVER = 'https://works-enrolled-flag-relevance.trycloudflare.com';
   var serverUrl = null;
   var vapidKey = null;
   var installPrompt = null;
