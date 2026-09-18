2581d6c26c2f9367e07cdc6dcb12d2064cdda4bf
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-09-18 06:00:13 +0300

    infra: tunnel URL 2026-09-18 06:00

--- chat-widget.js
+++ chat-widget.js
@@ -7,7 +7,7 @@
 (function () {
   'use strict';
 
-  var DEFAULT_SERVER = 'https://lover-significantly-sherman-dependent.trycloudflare.com'; // подменяется start_server.ps1 на туннель
+  var DEFAULT_SERVER = 'https://plastic-printed-specials-bidding.trycloudflare.com'; // подменяется start_server.ps1 на туннель
   var serverUrl = null;
 
   function detectReportId() {
--- js/pwa.js
+++ js/pwa.js
@@ -6,7 +6,7 @@
 (function (global) {
   'use strict';
   var SW_PATH = 'sw.js';
-  var DEFAULT_SERVER = 'https://lover-significantly-sherman-dependent.trycloudflare.com';
+  var DEFAULT_SERVER = 'https://plastic-printed-specials-bidding.trycloudflare.com';
   var serverUrl = null;
   var vapidKey = null;
   var installPrompt = null;
