57e3c2180278316b4e421a02fb376dd764b59b23
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-09-18 14:30:15 +0300

    infra: tunnel URL 2026-09-18 14:30

--- chat-widget.js
+++ chat-widget.js
@@ -7,7 +7,7 @@
 (function () {
   'use strict';
 
-  var DEFAULT_SERVER = 'https://api.trycloudflare.com'; // подменяется start_server.ps1 на туннель
+  var DEFAULT_SERVER = 'https://verified-measuring-highland-worth.trycloudflare.com'; // подменяется start_server.ps1 на туннель
   var serverUrl = null;
 
   function detectReportId() {
