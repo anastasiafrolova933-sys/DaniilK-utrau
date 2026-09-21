fcdc17eb3a43a4323e7e5edbb7c8a87ac4ce964c
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-09-21 06:00:13 +0300

    infra: tunnel URL 2026-09-21 06:00

--- chat-widget.js
+++ chat-widget.js
@@ -7,7 +7,7 @@
 (function () {
   'use strict';
 
-  var DEFAULT_SERVER = 'https://soc-their-podcasts-mar.trycloudflare.com'; // подменяется start_server.ps1 на туннель
+  var DEFAULT_SERVER = 'https://series-comedy-mike-sterling.trycloudflare.com'; // подменяется start_server.ps1 на туннель
   var serverUrl = null;
 
   function detectReportId() {
--- js/pwa.js
+++ js/pwa.js
@@ -6,7 +6,7 @@
 (function (global) {
   'use strict';
   var SW_PATH = 'sw.js';
-  var DEFAULT_SERVER = 'https://soc-their-podcasts-mar.trycloudflare.com';
+  var DEFAULT_SERVER = 'https://series-comedy-mike-sterling.trycloudflare.com';
   var serverUrl = null;
   var vapidKey = null;
   var installPrompt = null;
