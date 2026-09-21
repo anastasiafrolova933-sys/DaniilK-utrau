f0574edf6285e25c1927a452289c814f8c6ec238
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-09-21 14:40:24 +0300

    infra: tunnel URL 2026-09-21 14:40

--- js/pwa.js
+++ js/pwa.js
@@ -6,7 +6,7 @@
 (function (global) {
   'use strict';
   var SW_PATH = 'sw.js';
-  var DEFAULT_SERVER = 'https://api.trycloudflare.com';
+  var DEFAULT_SERVER = 'https://experiment-enrolled-elections-needle.trycloudflare.com';
   var serverUrl = null;
   var vapidKey = null;
   var installPrompt = null;
