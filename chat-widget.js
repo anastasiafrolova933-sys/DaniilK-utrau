bc553b3dfb489fb2bf0b13a0b6bb2dcd607b41a5
danyaklimenko2018-png <anastasiafrolova933@gmail.com>
2026-09-23 06:00:13 +0300

    infra: tunnel URL 2026-09-23 06:00

--- chat-widget.js
+++ chat-widget.js
@@ -7,7 +7,7 @@
 (function () {
   'use strict';
 
-  var DEFAULT_SERVER = 'https://breaks-initially-muslim-gdp.trycloudflare.com'; // подменяется start_server.ps1 на туннель
+  var DEFAULT_SERVER = 'https://invited-heaven-interesting-apache.trycloudflare.com'; // подменяется start_server.ps1 на туннель
   var serverUrl = null;
 
   function detectReportId() {
